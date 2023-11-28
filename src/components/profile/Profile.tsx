6e86f5ddc429e6fd363ea81a00671f80eb2f67a1
Vital <Vitalii8881878@gmail.com>
2023-11-27 21:28:45 -0500

    add to profile 3+1 folder

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -6,11 +6,7 @@ import {MyPost} from "./myPost/MyPost";
 export const Profile = () => {
     return (
         <section className="section">
-            <div>
-
-                <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2116175301.1700524800&semt=ais"
-                     alt="Mountan"/>
-            </div>
+            <Profile/>
             <div> avatar</div>
             <FirstPost/>
             <MyPost/>
---
+++ src/components/profile/profilePicture/ProfilePicture.tsx
@@ -0,0 +1,15 @@
+import {FirstPost} from "../post/FirstPost";
+import {MyPost} from "../myPost/MyPost";
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div>
+
+            <img src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.2116175301.1700524800&semt=ais"
+                 alt="Mountan"/>
+        </div>
+
+
+    )
+}
