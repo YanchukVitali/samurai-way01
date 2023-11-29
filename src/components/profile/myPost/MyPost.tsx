f1d8e8b1a6f3a886fe2f2a4f0763ea568562c749
Vital <Vitalii8881878@gmail.com>
2023-11-28 22:07:44 -0500

    refactor Post.tsx need change text

--- src/components/profile/myPost/MyPost.tsx
+++ src/components/profile/myPost/MyPost.tsx
@@ -4,8 +4,9 @@ import {Post} from "../post/Post";
 
 export const MyPost = () => {
     return (
+        // need refactor this component  because i dont like
         <div>
-            <div className="post">My post</div>
+            <div className="post">My post <input type="text"/></div>
             <Post/>
             <Post/>
             <Post/>
--- src/components/profile/post/Post.tsx
+++ src/components/profile/post/Post.tsx
@@ -3,7 +3,7 @@ import "./Post.css"
 
 export const Post = () => {
     return (
-        <div className="Post">
+        <div className="post">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjYMzf_2M2DHKW2igKtQo3cc2IHcj7FzB-pg&usqp=CAU" alt="imeg profile"/>
             Post 1
         </div>
