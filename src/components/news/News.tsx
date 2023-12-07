7c870585be8630720eccf8bb8f24213c44efc059
Vital <Vitalii8881878@gmail.com>
2023-12-06 20:52:39 -0500

    refactor

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import n from "./News.module.css"
 export const News = () => {
     return (
-        <div>News</div>
+        <div className={n.div}>
+            <h1>News</h1>
+           <p>Four Russian soldiers charged with war crimes for torturing US citizen in Ukraine</p>
+        </div>
+        
     )
 }
